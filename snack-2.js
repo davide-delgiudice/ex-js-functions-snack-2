c0bffa3a3ae50033d1b23060a1e84b414488f26b
davide-delgiudice <dav.del.giudice@gmail.com>
2025-07-16 13:16:25 +0200

    [UPD] snack 3 completato

--- snack-2.js
+++ snack-2.js
@@ -19,8 +19,23 @@ const somma = (a, b) => a+b;
 console.log(somma(1, 4));
 
 
+
 // snack 2
 
 const quadrato = (numero) => numero*numero;
 
 console.log(quadrato(4));
+
+
+
+//  snack 3
+
+function eseguiOperazione(a, b, operatore) {
+    return operatore (a, b);
+}
+
+function moltiplica (x, y) {
+    return x*y;
+}
+
+console.log(eseguiOperazione(5, 7, moltiplica));
