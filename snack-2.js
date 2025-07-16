300f8660da8eacb2f2769c1c834e6918daf8cac5
davide-delgiudice <dav.del.giudice@gmail.com>
2025-07-16 12:11:07 +0200

    [UPD] snack 1 completato

---
+++ snack-2.js
@@ -0,0 +1,24 @@
+// snack 1
+
+function somma(primoNumero, secondoNumero) {
+    return primoNumero + secondoNumero;
+}
+
+console.log(somma(1,4));
+
+
+const sommaAnonima = function(a, b) {
+    return a + b;
+}
+
+console.log(sommaAnonima(34, 21));
+
+
+const sommaFreccia = (a, b) => a+b;
+
+console.log(sommaFreccia(20, 12));
+
+
+
+// snack 2
+
